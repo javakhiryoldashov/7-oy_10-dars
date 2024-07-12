0614696351ac78630460f785d9642092e5c5ecee
javokhiryoldashov <yuldoshovjavohir3@gmail.com>
2024-07-13 03:42:19 +0500

    fenished

---
+++ src/App.jsx
@@ -0,0 +1,125 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Routes, useNavigate, Route, resolvePath } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { useDispatch, useSelector } from "react-redux";
+import { deletePost, addPosts } from "./store/postsSlice";
+
+import { api } from "./api";
+
+function App() {
+  const posts = useSelector((store) => store.postsReducer.posts);
+  const dispatch = useDispatch();
+
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const [deleteLoading, setDeleteLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await api.get("/posts");
+        dispatch(addPosts(response.data));
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPosts();
+  }, []);
+
+  useEffect(() => {
+    const filteredResults = posts.filter(
+      (post) =>
+        post.body.toLowerCase().includes(search.toLowerCase()) ||
+        post.title.toLowerCase().includes(search.toLowerCase())
+    );
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const id = posts.length ? Number(posts[posts.length - 1].id) + 1 : 1;
+    const datetime = format(new Date(), "MMMM dd, yyyy pp");
+    const newPost = {
+      id: String(id),
+      title: postTitle,
+      datetime,
+      body: postBody,
+    };
+
+    try {
+      await api.post("/posts", newPost);
+      dispatch(addPosts(newPost));
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setPostTitle("");
+    setPostBody("");
+    navigate("/");
+  };
+
+  const handleDelete = async (id) => {
+    setDeleteLoading(true);
+    try {
+      await api.delete(`/posts/${id}`);
+      dispatch(deletePost(id));
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setDeleteLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav search={search} setSearch={setSearch} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <Routes>
+        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route
+          path="/post"
+          element={
+            <NewPost
+              handleSubmit={handleSubmit}
+              postTitle={postTitle}
+              setPostTitle={setPostTitle}
+              postBody={postBody}
+              setPostBody={setPostBody}
+            />
+          }
+        />
+        <Route
+          path="/post/:id"
+          element={<PostPage posts={posts} handleDelete={handleDelete} />}
+        />
+        <Route path="/about" component={<About />} />
+        <Route path="*" component={<Missing />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PostPage.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useParams, Link, Form } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { editPost } from "../store/postsSlice";
+import ClipLoader from "react-spinners/ClipLoader";
+import { fi } from "date-fns/locale/fi";
+
+const PostPage = ({ posts, handleDelete }) => {
+  const [edit, setEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const { id } = useParams();
+  const post = posts.find((post) => post.id.toString() === id);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (post) {
+      setTitle(post.title);
+      setBody(post.body);
+    }
+  }, [post]);
+
+  async function handleEdit() {
+    setLoading(true);
+    try {
+      await api.put(`/posts/${post.id}`, {
+        title,
+        body,
+      });
+      dispatch(editPost({ ...post, title, body }));
+      setEdit(!edit);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  }   
+
+  const navigate = useNavigate();
+
+  return (
+    <main className="PostPage">
+      <article className="post">
+        {post && (
+          <>
+            {edit ? (
+              <form>
+                <input
+                  type="text"
+                  name={"title"}
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                  rows={"5"}
+                  name="body"
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                />
+              </form>
+            ) : (
+              <>
+                <h2>{post.title}</h2>
+                <p className="postDate">{post.datetime}</p>
+                <p className="postBody">{post.body}</p>
+              </>
+            )}
+            <div className="buttons">
+              <button className="danger" onClick={() => handleDelete(post.id)}>
+                Delete
+              </button>
+
+              <button
+                onClick={() => {
+                  if (edit) {
+                    // dispatch(editPost({ ...post, title, body }));
+                    handleEdit();
+                  } else {
+                    setEdit(!edit);
+                  }
+                }}
+              >
+                {edit ? <>Save</> : "Edit"}
+                <ClipLoader
+                  loading={loading}
+                  size={20}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                  cssOverride={{ borderColor: "white", marginLeft: "0.2em" }}
+                />
+              </button>
+
+              <button onClick={() => navigate(-1)}>Cancel</button>
+            </div>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well, that's disappointing.</p>
+            <p>
+              <Link to="/">Visit Our Homepage</Link>
+            </p>
+          </>
+        )}
+      </article>
+    </main>
+  );
+};
+
+export default PostPage;
